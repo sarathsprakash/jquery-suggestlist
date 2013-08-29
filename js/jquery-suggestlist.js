6f949f5444a8a989336ff1e4f1c02c529f7f4dff
sarathsprakash <sarathsprakash@gmail.com>
2013-08-29 17:59:08 +0530

    Update jquery-suggestlist.js

--- js/jquery-suggestlist.js
+++ js/jquery-suggestlist.js
@@ -148,23 +148,23 @@
 		},
 
 		updateLi: function( event ) {
-			/*if ( event ) {
+			if ( event ) {
 				var keyVal = String.fromCharCode( event.keyCode ).toLowerCase();
 				if ( event.ctrlKey || ! /^[0-9a-z ]$/.test( keyVal ) ) {
 					return;
 				}
-			}*/
+			}
 
 			var val = $.trim( this.element.val() ).replace(/\s+/, ' '),
 				$li = this.picker.find( 'li' ),
 				$selected = $li.filter( '.suggestlist-selected' ).first();
-		       /*	if ( val === $selected.text() ) {
+		       	if ( val === $selected.text() ) {
 				return;
-			}*/
+			}
 			//if ( $.inArray( val, this.options.list ) === -1 ) {
 			//	return false;
 		     //	}
-			//$selected.removeClass( 'suggestlist-selected' );
+			$selected.removeClass( 'suggestlist-selected' );
 			this.picker.find( 'li' ).each( function( i, elem ) {
 				if ( $( elem ).text().match("^"+val)) {
 					
