3721b9def3bfd46dd151adf4e92f82af470c28be
Sarath S <sarathsprakash@gmail.com>
2013-08-30 10:10:47 +0530

    Update jquery-suggestlist.js

--- js/jquery-suggestlist.js
+++ js/jquery-suggestlist.js
@@ -149,18 +149,18 @@
 		},
 
 		updateLi: function( event ) {
-		      	/*  
+		       
 		      	 //Removed Code
-		      	if ( event ) {
+		      //	if ( event ) {
 				var keyVal = String.fromCharCode( event.keyCode ).toLowerCase();
 				if ( event.ctrlKey || ! /^[0-9a-z ]$/.test( keyVal ) ) {
 					return;
 				}
-			}*/
+		       //	}*/
                       
 		        	var val = $.trim( this.element.val() ).replace(/\s+/, ' '),
 				$li = this.picker.find( 'li' ),
-			       	$selected = $li.filter('.suggestlist-selected').first();
+			       	
 		       	       /*	
 		       	         //Removed Code
 		       	       if ( val === $selected .text() ) {
@@ -186,6 +186,7 @@
 				$( elem ).removeClass('suggestlist-selected');
 				}
 			} );
+			$selected = $li.filter('.suggestlist-selected').first();
 			if ( event ) {
 				this.show();
 			}
